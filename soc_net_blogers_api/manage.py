d612a231ac745644ea8b72dd8d5d8b14c5fda4ae
Виктор Шустров <shustrov19@gmail.com>
2023-10-04 20:05:28 +0300

    some fix

--- soc_net_blogers_api/manage.py
+++ soc_net_blogers_api/manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soc_net_blogers_api.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'soc_net_blogers_api.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
